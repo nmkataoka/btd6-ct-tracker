84b689c2d4b75812e02d26d17789952d7b87e872
Nolan Kataoka <nmkataoka@gmail.com>
2022-08-05 18:13:16 -0400

    feat: set up initial discord bot

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'airbnb',
+      'airbnb-typescript',
+      'prettier',
+    ],
+    parserOptions: {
+      ecmaVersion: 2021,
+      project: './tsconfig.json',
+    },
+    plugins: ['prettier', '@typescript-eslint'],
+    rules: {
+      'object-curly-spacing': ['error', 'always'],
+      indent: ['off'],
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define': ['error'],
+      "import/prefer-default-export": 0
+    },
+    env: {
+      // change as necessary
+      node: true,
+    },
+  };
